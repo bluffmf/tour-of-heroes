a32d2f629ac9b2b80eb4cbaf4d9a9aed6490df57
Dmitry Troyan <Dmitry.Troyan@volo.global>
2017-05-03 15:45:46 +0400

    fix class hero

--- src/app/in-memory-data.service.js
+++ src/app/in-memory-data.service.js
@@ -1,19 +1,32 @@
 "use strict";
+var hero_1 = require("./hero");
 var InMemoryDataService = (function () {
     function InMemoryDataService() {
     }
     InMemoryDataService.prototype.createDb = function () {
+        // let heroes = [
+        //   { id: 11, name: 'Pianino' },
+        //   { id: 12, name: 'Franclin' },
+        //   { id: 13, name: 'Super Mario' },
+        //   { id: 14, name: 'Batman' },
+        //   { id: 15, name: 'Chubaka' },
+        //   { id: 16, name: 'Satana' },
+        //   { id: 17, name: 'Splinter' },
+        //   { id: 18, name: 'Legalas' },
+        //   { id: 19, name: 'Zorro' },
+        //   { id: 20, name: 'Marley' }
+        // ];
         var heroes = [
-            { id: 11, name: 'Pianino' },
-            { id: 12, name: 'Franclin' },
-            { id: 13, name: 'Super Mario' },
-            { id: 14, name: 'Batman' },
-            { id: 15, name: 'Chubaka' },
-            { id: 16, name: 'Satana' },
-            { id: 17, name: 'Splinter' },
-            { id: 18, name: 'Legalas' },
-            { id: 19, name: 'Zorro' },
-            { id: 20, name: 'Marley' }
+            new hero_1.Hero(11, 'Pianino'),
+            new hero_1.Hero(12, 'Franclin'),
+            new hero_1.Hero(13, 'Super Mario'),
+            new hero_1.Hero(14, 'Batman'),
+            new hero_1.Hero(15, 'Chubaka'),
+            new hero_1.Hero(16, 'Satana'),
+            new hero_1.Hero(17, 'Splinter'),
+            new hero_1.Hero(18, 'Legalas'),
+            new hero_1.Hero(19, 'Zorro'),
+            new hero_1.Hero(20, 'Marley')
         ];
         return { heroes: heroes };
     };
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,18 +1,31 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
+    // let heroes = [
+    //   { id: 11, name: 'Pianino' },
+    //   { id: 12, name: 'Franclin' },
+    //   { id: 13, name: 'Super Mario' },
+    //   { id: 14, name: 'Batman' },
+    //   { id: 15, name: 'Chubaka' },
+    //   { id: 16, name: 'Satana' },
+    //   { id: 17, name: 'Splinter' },
+    //   { id: 18, name: 'Legalas' },
+    //   { id: 19, name: 'Zorro' },
+    //   { id: 20, name: 'Marley' }
+    // ];
     let heroes = [
-      { id: 11, name: 'Pianino' },
-      { id: 12, name: 'Franclin' },
-      { id: 13, name: 'Super Mario' },
-      { id: 14, name: 'Batman' },
-      { id: 15, name: 'Chubaka' },
-      { id: 16, name: 'Satana' },
-      { id: 17, name: 'Splinter' },
-      { id: 18, name: 'Legalas' },
-      { id: 19, name: 'Zorro' },
-      { id: 20, name: 'Marley' }
+      new Hero(11, 'Pianino'),
+      new Hero(12, 'Franclin'),
+      new Hero(13, 'Super Mario'),
+      new Hero(14, 'Batman'),
+      new Hero(15, 'Chubaka'),
+      new Hero(16, 'Satana'),
+      new Hero(17, 'Splinter'),
+      new Hero(18, 'Legalas'),
+      new Hero(19, 'Zorro'),
+      new Hero(20, 'Marley')
     ];
     return {heroes};
   }
